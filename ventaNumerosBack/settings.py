c2dd80d54094ea619dc88cde3678d4fe9283b8bb
B3RZVS <79112396+B3RZVS@users.noreply.github.com>
2024-10-17 08:02:46 -0300

    previo a la subida

--- ventaNumerosBack/settings.py
+++ ventaNumerosBack/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,11 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(du4kmronp)7mkcyi)*m)go4f-4o_^w$j+9$28^c1bkjqr$*30'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://192.168.68.52:5173",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
@@ -83,10 +87,9 @@ WSGI_APPLICATION = 'ventaNumerosBack.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
